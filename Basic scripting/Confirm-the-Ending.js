9e68b71e0cc45b28e1c3fe26058a7b7e805620d6
WEremite <weremite@gmail.com>
2020-12-14 17:34:59 +0200

    Rename files
    Rename files

--- Basic scripting/Confirm-the-Ending.js
+++ Basic scripting/Confirm-the-Ending.js
@@ -1,6 +1,7 @@
 //my variation
 function confirmEnding(str, target) {
     if (str.substring(str.length - target.length) === target) {
+    if (str.substring(str.length - target.length) === target) {
         return true;
     } else {
         return false;
