a62256c624156d335fbe1f510eeff855d199983e
WEremite <weremite@gmail.com>
2020-12-19 21:08:43 +0200

    Backup #10

---
+++ LeetCode/Add-Two-Numbers.js
@@ -0,0 +1,47 @@
+/*
+You are given two non-empty linked lists representing
+two non-negative integers. The digits are stored in
+reverse order, and each of their nodes contains a
+single digit. Add the two numbers and return the
+sum as a linked list.
+
+You may assume the two numbers do not contain any
+leading zero, except the number 0 itself.
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+
+
+
+Constraints:
+
+    The number of nodes in each linked list is in the range [1, 100].
+    0 <= Node.val <= 9
+    It is guaranteed that the list represents a number that does not have leading zeros.
+
+ */
+var addTwoNumbers = function(l1, l2) {
+    if (l1.length === 1 && l1[0] === 0 && l2.length === 1 && l2[0] === 0) {
+        return [0];
+    }
+
+    let l3 = (parseInt(l1.reverse().join('')) + parseInt(l2.reverse().join(''))).toString().split('');
+
+    return l3.reverse().map(Number);
+};
+
+console.log(addTwoNumbers([2,4,3], [5,6,4])); //Output: [7,0,8] Explanation: 342 + 465 = 807.
+console.log(addTwoNumbers([9,9,9,9,9,9,9], [9,9,9,9])); //Output: [8,9,9,9,0,0,0,1]
+console.log(addTwoNumbers([0], [0])); //Output: [0]
+
