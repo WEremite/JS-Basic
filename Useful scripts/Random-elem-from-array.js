e28937c6f9af9939d388ca5d894240b9c42ee009
WEremite <weremite@gmail.com>
2021-03-23 17:14:10 +0200

    Add Useful Script folder for future

---
+++ Useful scripts/Random-elem-from-array.js
@@ -0,0 +1,25 @@
+const arr = [
+  "One",
+  "morning",
+  "when",
+  "Gregor",
+  "Samsa",
+  "woke",
+  "from",
+  "troubled",
+  "dreams",
+  "he",
+  "found",
+  "himself",
+  "transformed",
+  "in",
+  "his",
+  "bed",
+  "into",
+  "a",
+  "horrible",
+  "vermin",
+];
+let value = Math.floor(Math.random() * arr.length);
+
+console.log(value, arr[value]);
